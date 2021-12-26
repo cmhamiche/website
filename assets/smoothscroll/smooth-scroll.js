6abd7d4d12012691f346e8be2eef56ed41e50709
cmh <32405217+cmhamiche@users.noreply.github.com>
2021-12-26 21:23:34 +0100

    Update smooth-scroll.js

--- assets/smoothscroll/smooth-scroll.js
+++ assets/smoothscroll/smooth-scroll.js
@@ -1,6 +1,6 @@
 //
 // SmoothScroll for websites v1.4.5 (Balazs Galambosi)
-// http://www.smoothscroll.net/
+// https://www.smoothscroll.net/
 //
 // Licensed under the terms of the MIT license.
 //
